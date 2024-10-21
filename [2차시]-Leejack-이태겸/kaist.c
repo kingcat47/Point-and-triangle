ace8acdbbf84499097197028f1a4e0a77affea47
orangejack <79419803+kingcat47@users.noreply.github.com>
2024-10-22 01:07:03 +0900

    Update kaist.c
    안녕하세요 C언어 레벨2 수강 중인 학생입니다.
    과제를 제출하려고 하는데 pdf에서는 이차원 평면 위의 점 하나를 나타낼 수 있는 구조체 태그 Point를 만들고, 세 점의 좌표를 입력받아 세 점으로 삼각형을 만들 수 있으면 “true”를, 만들 수 없으면 “false”를 출력합니다. 라고 나와 있습니다.
    하지만 설명글에는 세 점사이의 거리에 따라 삼각형 완성 여부를 구별할 수 있는 방법만을 알려주는데 세 점사이의 거리를 구하고 대소만 비교해서 참과 거짓을 출력하는 게 맞나요?
    이렇게되면 예를 들어 입력이 A(0,0), B(2,2), C(4,4) 로 주어졌을때 세 점들은 x=y라는 한 직선위에 있는 점들이라 삼각형이 만들어지지 못함에도 불구하고 거리만으로 계산하기 때문에 선분 AB = 2, 선분 BC = 2, 선분 AC는 4가 되어
    pdf설명에 나와 있는 if 세 변의거리 a,b,c가 a<=b<=c 일때 (a+b) > c 이면 삼각형이 성립한다에 따라서 true가 출력이 됩니다. 이는 처음에 말했던 출력조건(세 점의 좌표를 입력받아 세 점으로 삼각형을 만들 수 있으면 “true”)과 모순이 생기기 때문에 기울기까지 고려를 해서 코드를 작성 해야하는지 아니면 pdf에 나와있는 거리를 구한뒤 대소를 구분하는 걸로 끝내야하는건지 궁금합니다.
    그리고 배운 열거형을 사용하지 않은것(필요성을 못 느낌)과 당연히 괜찮겠지 하면서 math라이브러리를 사용했는데(제곱근과 거듭제곱연산을 쉽게하기위해서) 혹시 안 되는 거라면 같이 알려주시면 감사 할 것 같습니다. 
    
    
    
    이 질문에 대한 답변으로 음 잘 찾았네 고려해서 만드셈 이라는 답변을 받아서 기울기고려 추가하고 기존 check1조건에서 abc순서 대소를 안맞춰주고 비교했던거 같아서 바꿔줌

--- [2차시]-Leejack-이태겸/kaist.c
+++ [2차시]-Leejack-이태겸/kaist.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
-
-//제곱근과 거듭제곱을 쉽게할수있도록 
-//해주는 함수가 있는 파일이다
-//당연히 사용가능한걸로 알고 사용하겠음
-#include<math.h> 
-
+#include<math.h>
 
 //포인터객체
 typedef struct{
@@ -20,10 +15,37 @@ int d(Point x, Point y){
     //pow는 n제곱 구하는 함수 
 }
 
-//삼각형조건확인하기
-const char* last(int a,int b,int c){
-    return (a+b)>c ? "true" : "false";
+//삼각형조건확인하기(거리)
+int last1(int a,int b,int c){
+    //a<=b<=c 맞춰주기
+
+    // 세 변 중 가장 큰 값 찾기
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+
+    // 세 변 중 가장 작은 값 찾기
+    int min = a;
+    if (b < min) min = b;
+    if (c < min) min = c;
+
+    // 중간 값 찾기
+    int mid = a + b + c - max - min;
+
+    return (min+mid)>max ? 1 : 0;
 }
+
+//삼각형조건확인하기(기울기)
+double last2(Point x, Point y){
+    //나누는 값이 0이면 오류날수 있어서추가
+    //무한대를 안배워서 모르겠는데 검색해보니 무한대로 표현한다길래 INFINITY넣음
+    if(x.x == y.x){
+        return INFINITY;
+    }
+
+    return (double)(x.y - y.y) / (x.x - y.x);
+}
+
 
 int main(){
     Point a,b,c;
@@ -32,12 +54,36 @@ int main(){
     scanf("%d %d",&b.x ,&b.y);
     scanf("%d %d",&c.x ,&c.y);
 
+    //거리
     int d1,d2,d3;
     d1 = d(a,b);
     d2 = d(a,c);
     d3 = d(c,b); 
 
-    printf("%s",last(d1,d2,d3));
+    //기울기
+    double m1,m2,m3;
+    m1 = last2(a,b);
+    m2 = last2(a,c);
+    m3 = last2(c,b);
+
+    int check1 = last1(d1,d2,d3); // 거리
+    int check2; // 기울기
+
+    if((m1 == m2) && (m2 == m3)){
+        check2 = 0;
+    }
+    else{
+        check2 = 1;
+    }
+
+    //check 2개가 모두 참이면 true 출력
+    if ((check1 == 1) && (check2 == 1)  ) {
+        printf("true");
+    }
+    //아니라면 false 출력
+    else {
+        printf("false");
+    }
 
     return 0;
 }
